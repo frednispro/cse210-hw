0bfd59b3a35b544dc8e45e2a692879569dfc4b0a
Fred Nshimiyimana <fredynshimiye92@gmail.com>
2025-05-13 14:04:09 -0600

    added code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,5 +6,10 @@ class Program
     {
        Console.WriteLine("x is 10");
        // This is a comment.
+     //this is a comment 
+     // this is a comment
+       Console.WriteLine("Hello, World!");
+       Console.WriteLine("x is 10");
+       // This is a comment.
     }
 }
